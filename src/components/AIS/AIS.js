e31b83c095650125c8dbd82eace030be3b1c0096
Andre de Oliveira <oliveira.andrede@gmail.com>
2017-05-30 16:06:36 -0300

    add redes sociais

--- src/components/AIS/AIS.js
+++ src/components/AIS/AIS.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { PageTitle } from '../content/PageTitle/PageTitle'
-import { Carreira } from './Carreira/Carreira'
 import { Contato } from './Contato/Contato'
 import './AIS.css'
 
