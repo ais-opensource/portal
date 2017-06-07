23ef012b092bc1f215180965ced1862d2c496873
Andre de Oliveira <oliveira.andrede@gmail.com>
2017-06-06 22:22:27 -0300

    Add event

--- src/components/ui-elements/AISActionButton.js
+++ src/components/ui-elements/AISActionButton.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import * as ReactGA from 'react-ga'
 import './AISActionButton.css'
 export class AISActionButton extends React.Component {
+
+handleClickEvent(eventName, event) {
+
+   ReactGA.event({
+     category: 'Ação de Botão',
+     action: eventName
+   })
+}
 
   render() {
     const url = this.props.url ? this.props.url : '#' + this.props.id
     return (
-      <a href={url} className="ais-action-button">{this.props.text}</a>
+      <a href={url} onClick={this.handleClickEvent.bind(this, this.props.text)} className="ais-action-button">{this.props.text}</a>
     )
   }
 }
